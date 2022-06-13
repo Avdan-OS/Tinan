abbd13190871788c39b104cfbb5c0cd71d0b56d1
Padmanabh <kharat82@gmail.com>
2022-06-13 12:30:09 +0530

    setup for goos

---
+++ src/features/commands.js
@@ -0,0 +1,22 @@
+export let commands = [
+    {
+        name: "",
+        discription: "",
+        // parameter that function needs
+        parameters: [
+            {
+                name: "massage",
+                required: true,
+                discription: "massage"
+            },
+            {
+                name: "massage2",
+                required: true,
+                discription: "massage2"
+            }
+        ],
+        handlerfunction: (parameters) => {
+
+        }
+    }
+]
