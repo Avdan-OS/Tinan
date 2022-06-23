36bf730f95111d27bb81267822ce826c8e3031ce
Golem64 <65229557+Golem642@users.noreply.github.com>
2022-06-23 12:12:10 +0200

    Update poll.js
    Added /poll end which is working properly

--- src/commands/slash/poll.js
+++ src/commands/slash/poll.js
@@ -99,8 +99,14 @@ module.exports = {
           name: 'id',
           description: 'ID of the poll',
           required: true,
+          type: Constants.ApplicationCommandOptionTypes.INTEGER
+        },
+        {
+          name: 'reason',
+          description: 'Reason for ending the poll',
+          required: false,
           type: Constants.ApplicationCommandOptionTypes.STRING
-        }
+        },
       ],
 		},
   ],
@@ -112,87 +118,98 @@ module.exports = {
       title: `Poll: ${interaction.options.getString('subject')}`,
       description: `Poll ends <t:${timestamp}:R>`,
       color: 'BLUE',
-      footer: "Poll id: "+pollId.toString(),
-      fields: [
-        {
-          name: 'Total votes',
-          value: "0",
-          inline: true,
-        },
-      ],
     });
-    if (interaction.member.permissions.has(Permissions.FLAGS.MANAGE_EVENTS)) {
-      embed.setAuthor({
-        name: interaction.member.displayName,
-        iconURL: interaction.member.displayAvatarURL()
-      })
-      const getButton = (buttonID) => interaction.options.getString(buttonID);
-      const row1 = new MessageActionRow();
-      const row2 = new MessageActionRow();
-      
+    if ( interaction.member.permissions.has(Permissions.FLAGS.MANAGE_EVENTS)) {
+      if ( interaction.options._subcommand == "create" ) {
+        embed.addFields([
+          {
+            name: 'Total votes',
+            value: "0",
+            inline: true,
+          }
+        ])
+        embed.setAuthor({
+          name: interaction.member.displayName,
+          iconURL: interaction.member.displayAvatarURL()
+        })
+        embed.setFooter({ text: "Poll id: "+pollId.toString() })
+        const getButton = (buttonID) => interaction.options.getString(buttonID);
+        const row1 = new MessageActionRow();
+        const row2 = new MessageActionRow();
 
-      for (let i = 1; i < 6; i++) { 
-        if (getButton(i.toString())) {
-          row1.addComponents(
-            new MessageButton()
+
+        for (let i = 1; i < 6; i++) { 
+          if (getButton(i.toString())) {
+            row1.addComponents(
+              new MessageButton()
+                .setCustomId('poll_' + pollId.toString() + '_' + i.toString())
+                .setLabel(getButton(i.toString()))
+                .setStyle('SECONDARY')
+            ) 
+          }
+        }
+
+        for (let i = 6; i < 11; i++) { 
+          if (getButton(i.toString())) {
+            row2.addComponents(
+              new MessageButton()
               .setCustomId('poll_' + pollId.toString() + '_' + i.toString())
               .setLabel(getButton(i.toString()))
               .setStyle('SECONDARY')
-          ) 
-        }
-      }
-
-      for (let i = 6; i < 11; i++) { 
-        if (getButton(i.toString())) {
-          row2.addComponents(
-            new MessageButton()
-            .setCustomId('poll_' + pollId.toString() + '_' + i.toString())
-            .setLabel(getButton(i.toString()))
-            .setStyle('SECONDARY')
-          )
-        }
-      }
-      const rows = [];
-      if (row1.components.length) rows.push(row1);
-      if (row2.components.length) rows.push(row2);
-      pollsList[pollId] = {};
-      global.getLabel = function (pollID, buttonID) { if (pollID == pollId) return interaction.options.getString(buttonID) };
-      let mention = ' ';
-      if (interaction.options.getBoolean('mention')) mention = '@everyone';
-      interaction.channel.send({ content: mention, embeds: [embed], components: rows }).then(message => {
-        interaction.reply({ content: 'The poll has been created', ephemeral: true })
-        const collector = interaction.channel.createMessageComponentCollector({ time: interaction.options.getInteger('time') * 1000 });
-        collector.on('collect', () => {
-          embed.fields[0].value = Object.values(pollsList[pollId]).length.toString()
-          message.edit({ embeds: [embed] })
-        });
-        collector.on('end', () => {
-          if (pollsList[pollId]) {
-            let winners = [];
-            let maxLength = 1;
-            for (let i = 1; i < 11; i++) {
-              const currentLenght = Object.values(pollsList[pollId]).filter(x => x == 'poll_' + pollId.toString() + '_' + i.toString()).length;
-              if (currentLenght >= maxLength) {
-                if (currentLenght > maxLength) {
-                  maxLength = currentLenght;
-                  winners.pop();
-                }
-                winners.push(interaction.options.getString(i.toString()));
-              }
-            }
-            const totalVotes = embed.fields[0].value
-            //embed.setTitle(`Poll ended: ${interaction.options.getString('subject')}`)
-            embed.setDescription(`Poll ended <t:${timestamp}:R>`)
-            embed.setFields([])
-            if (winners.length == 1) embed.addFields([{ name: "Winner", value: `**${winners}** with **${maxLength}** votes`, inline: true }])
-            else if (winners.length > 1) embed.addFields([{ name: "Winners", value: `**${winners}** with **${maxLength}** votes each`, inline: true }])
-            else embed.setDescription(`There was no winner (no votes have been performed)`)
-            embed.addFields([{ name: "Total votes", value: totalVotes, inline: true }])
-            message.edit({ embeds: [embed], components: [] });
-            delete pollsList[pollId];
+            )
           }
+        }
+        const rows = [];
+        if (row1.components.length) rows.push(row1);
+        if (row2.components.length) rows.push(row2);
+        pollsList[pollId] = {};
+        global.getLabel = function (pollID, buttonID) { if (pollID == pollId) return interaction.options.getString(buttonID) };
+        let mention = ' ';
+        if (interaction.options.getBoolean('mention')) mention = '@everyone';
+        interaction.channel.send({ content: mention, embeds: [embed], components: rows }).then(message => {
+          interaction.reply({ content: 'The poll has been created', ephemeral: true })
+          const collector = interaction.channel.createMessageComponentCollector({ time: interaction.options.getInteger('time') * 1000 });
+          collector.client.on('interactionCreate', (interact) => {
+            if (interact.isCommand()) if (interact.commandName == "poll") if (interact.options._subcommand == "end") if (interact.options.getInteger("id") == pollId) collector.stop()
+          })
+          collector.on('collect', () => {
+            embed.fields[0].value = Object.values(pollsList[pollId]).length.toString()
+            message.edit({ embeds: [embed] })
+          });
+          collector.on('end', () => {
+            if (pollsList[pollId]) {
+              let winners = [];
+              let maxLength = 1;
+              for (let i = 1; i < 11; i++) {
+                const currentLenght = Object.values(pollsList[pollId]).filter(x => x == 'poll_' + pollId.toString() + '_' + i.toString()).length;
+                if (currentLenght >= maxLength) {
+                  if (currentLenght > maxLength) {
+                    maxLength = currentLenght;
+                    winners.pop();
+                  }
+                  winners.push(interaction.options.getString(i.toString()));
+                }
+              }
+              const totalVotes = embed.fields[0].value
+              //embed.setTitle(`Poll ended: ${interaction.options.getString('subject')}`)
+              embed.setDescription(`Poll ended <t:${timestamp}:R>`)
+              embed.setFields([])
+              if (winners.length == 1) embed.addFields([{ name: "Winner", value: `**${winners}** with **${maxLength}** votes`, inline: true }])
+              else if (winners.length > 1) embed.addFields([{ name: "Winners", value: `**${winners}** with **${maxLength}** votes each`, inline: true }])
+              else embed.setDescription(`There was no winner (no votes have been performed)`)
+              embed.addFields([{ name: "Total votes", value: totalVotes, inline: true }])
+              //embed.setFooter({ text: "" })
+              message.edit({ embeds: [embed], components: [] });
+              delete pollsList[pollId];
+            }
+          });
         });
-      });
+      } else {
+        embed.setTitle(`Poll \`${interaction.options.getInteger("id").toString()}\` was terminated by \`${interaction.member.user.tag}\``)
+        if (interaction.options.getString("reason")) embed.addFields([{ name: "Reason",  value: interaction.options.getString("reason"), inline: true }]).setDescription("")
+        else embed.setDescription("No reason specified")
+        interaction.reply({ embeds: [embed] })
+      }
     } else {
       embed.setTitle(`Error`)
       embed.setDescription(`You don't have sufficient permissions to execute that command`)
