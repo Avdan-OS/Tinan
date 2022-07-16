ec1309f6c0860cb6722e210e00b2281ca8834683
ANTR <68156188+ANTR3392@users.noreply.github.com>
2022-07-16 15:26:15 +0300

    Update cmdHandler.js
    commited using phone 💀

--- src/commands/cmdHandler.js
+++ src/commands/cmdHandler.js
@@ -73,7 +73,7 @@ module.exports = async (client) => {
       message.content.endsWith('in 100 seconds') &&
       message.content != 'this has been in 100 seconds'
     ) {
-      message.channel.send('hit the like button if you want to see more short videos like this thanks for watching and I will see you in the next one')
+      message.channel.send('hit the like button and subscribe if you want to see more short videos like this thanks for watching and I will see you in the next one')
     }
 
     if (!message.author.bot) {
